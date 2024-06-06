12096a73c3d9f7a9586efbe6d0008654e6987d04
Michael Zhou <michael.zhouzzy@gmail.com>
2024-06-06 00:17:43 -0700

    Translate full website to Chinese

--- src/playground.ts
+++ src/playground.ts
@@ -73,21 +73,21 @@ let INPUTS: {[name: string]: InputFeature} = {
 };
 
 let HIDABLE_CONTROLS = [
-  ["Show test data", "showTestData"],
-  ["Discretize output", "discretize"],
-  ["Play button", "playButton"],
-  ["Step button", "stepButton"],
-  ["Reset button", "resetButton"],
-  ["Learning rate", "learningRate"],
-  ["Activation", "activation"],
-  ["Regularization", "regularization"],
-  ["Regularization rate", "regularizationRate"],
-  ["Problem type", "problem"],
-  ["Which dataset", "dataset"],
-  ["Ratio train data", "percTrainData"],
-  ["Noise level", "noise"],
-  ["Batch size", "batchSize"],
-  ["# of hidden layers", "numHiddenLayers"],
+  ["显示测试数据", "showTestData"],
+  ["离散输出", "discretize"],
+  ["播放按钮", "playButton"],
+  ["步骤按钮", "stepButton"],
+  ["重置按钮", "resetButton"],
+  ["学习率", "learningRate"],
+  ["激活函数", "activation"],
+  ["正则化", "regularization"],
+  ["正则化率", "regularizationRate"],
+  ["问题类型", "problem"],
+  ["数据集", "dataset"],
+  ["训练数据比例", "percTrainData"],
+  ["噪声水平", "noise"],
+  ["批次大小", "batchSize"],
+  ["隐藏层数", "numHiddenLayers"],
 ];
 
 class Player {
@@ -696,9 +696,8 @@ function addPlusMinusControl(x: number, layerIdx: number) {
       .attr("class", "material-icons")
       .text("remove");
 
-  let suffix = state.networkShape[i] > 1 ? "s" : "";
   div.append("div").text(
-    state.networkShape[i] + " neuron" + suffix
+    state.networkShape[i] + " 神经元"
   );
 }
 
